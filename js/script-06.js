1f938d83b2131936a3d951e93915f01b68154ea5
cavidan <cavidaneken1@gmail.com>
2023-03-16 16:59:33 +0300

    16.03.23-2

--- js/script-06.js
+++ js/script-06.js
@@ -1,6 +1,22 @@
-const sayi=prompt("bir sayi giriniz: ");
-if (sayi!=0){
-    const sonuc=50/sayi;
-    alert("sonuç: "+ sonuc);
+/*
+ Javascript te === veya !== ifadelerinde hem değer hem tip kontrolü yapılır
+ == veya != ifadelerinde ise sadece değer kontrolü yapılır.
+ "0" !== 0   => true
+ "0" != 0    => false
+ "0" === 0   => false
+ "0" == 0    => true
+*/
+const sayi1 = Number(prompt("1.sayıyı giriniz"));
+const sayi2 = Number(prompt("2.sayıyı giriniz"));
+const sayi3 = Number(prompt("3.sayıyı giriniz"));
+const ort = (sayi1 + sayi2 + sayi3) / 3;
+if(ort>sayi1){
+    alert("Kâr");
 }
-
+else{
+    alert("Zarar");
+}
+if(sayi!=0){
+    const sonuc = 50/sayi;
+    alert("Sonuç:" + sonuc);
+}
